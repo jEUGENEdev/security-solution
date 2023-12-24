0148e772c0b69321296c852160a01ead5549389d
Eugene <genyamateyuk@gmail.com>
2023-12-25 01:01:38 +0300

    logic in profile

--- app/src/main/java/com/deveugene/securitysolution/activity/ProfileActivity.java
+++ app/src/main/java/com/deveugene/securitysolution/activity/ProfileActivity.java
@@ -1,11 +1,16 @@
 package com.deveugene.securitysolution.activity;
 
+import static android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.deveugene.securitysolution.R;
 
@@ -29,5 +34,19 @@ public class ProfileActivity extends AppCompatActivity {
         this.phone = findViewById(R.id.profilePhone);
         this.password = findViewById(R.id.profilePassword);
         this.confirmPassword = findViewById(R.id.profilePasswordConfirm);
+    }
+
+    public void clickProfileCancelButton(View view) {
+        finish();
+    }
+
+    public void clickAvatar(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    public void clickSaveButton(View view) {
+        finish();
+        Toast.makeText(this, "Изменения были сохранены!", Toast.LENGTH_LONG).show();
     }
 }
